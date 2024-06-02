e25343d06c00d928debb8d5a5329421768fcd87e
almerr <93062079+almerr@users.noreply.github.com>
2024-06-02 21:13:09 +0800

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,26 +1,27 @@
 class PATHS:
-    BASE_PATH = "/mnt/md0/rsna_abd/"
+    BASE_PATH = "/kaggle/working"
 
-    OUTPUT_BASE = f'{BASE_PATH}/KAGGLE_SUBMISSION/'
+    OUTPUT_BASE = f'{BASE_PATH}/KAGGLE_SUBMISSION'
 
-    CONTRAIL_MODEL_BASE = f'{BASE_PATH}/src_contrails/'
-    COAT_PRETRAINED = f'{BASE_PATH}/coat_pretrained/'
+    CONTRAIL_MODEL_BASE = f'{BASE_PATH}/src_contrails'
+    COAT_PRETRAINED = f'{BASE_PATH}/coat_pretrained'
 
-    SEGMENTOR_SAVE_FOLDER = f'{OUTPUT_BASE}/segmentation_data_v1/'
+    SEGMENTOR_SAVE_FOLDER = f'{OUTPUT_BASE}/segmentation_data_v1'
 
-    TOTAL_SEGMENTOR_FOLDER = f'{BASE_PATH}/TotalSegmentor/Totalsegmentator_dataset/'
-    TOTAL_SEGMENTOR_SAVE_FOLDER = f'{OUTPUT_BASE}/total_data_v1/'
+    TOTAL_SEGMENTOR_FOLDER = f'{BASE_PATH}/TotalSegmentor/Totalsegmentator_dataset'
+    TOTAL_SEGMENTOR_SAVE_FOLDER = f'{OUTPUT_BASE}/total_data_v1'
 
-    SEGMENTATION_MODEL_SAVE = f'{OUTPUT_BASE}/SEG_MODELS/'
+    SEGMENTATION_MODEL_SAVE = f'{OUTPUT_BASE}/SEG_MODELS'
 
     INFO_DATA_SAVE = f'{BASE_PATH}/KAGGLE_SUBMISSION/info_data1_processed_v1.csv'
 
-    THEO_DATA_PATH = f'{BASE_PATH}/theo_images/'
-    THEO_SAVE_PATH = f'{BASE_PATH}/KAGGLE_SUBMISSION/theo_data/'
+    THEO_DATA_PATH = f'{BASE_PATH}/theo_images'
+    THEO_SAVE_PATH = f'{BASE_PATH}/KAGGLE_SUBMISSION/theo_data'
 
-    OURDATA_VOL_SAVE_PATH = f'{BASE_PATH}/KAGGLE_SUBMISSION/our_data/'
+    OURDATA_VOL_SAVE_PATH = f'{BASE_PATH}/KAGGLE_SUBMISSION/our_data'
 
-    MODEL_SAVE = f'{OUTPUT_BASE}/models/'
+    MODEL_SAVE = f'{OUTPUT_BASE}/models'
+
     
 
 
